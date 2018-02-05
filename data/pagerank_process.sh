a03bf05b81772894e698e2d65556ccb3ce6f6cf7
Steven Brotz <sbrotz@caltech.edu>
2018-02-05 03:04:13 -0800

    Modified local testing script so that it makes some sense

--- data/pagerank_process.sh
+++ data/pagerank_process.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
-PYVERSION=python2.7 # Change this to however you invoke python
-COUNT=0
-while [ $COUNT -lt 50 ];
+i=0
+n_iterations=5
+input=input.txt
+python_ver=python2.7 # Change this to however you invoke python
+
+while [ $i -lt $n_iterations ]
 do
-$PYVERSION pagerank_map.py < input.txt | sort | $PYVERSION pagerank_reduce.py |
-$PYVERSION process_map.py | sort | $PYVERSION process_reduce.py > output.txt
-((COUNT++))
+    $python_ver pagerank_map.py < $input |
+    sort |
+    $python_ver pagerank_reduce.py |
+    $python_ver process_map.py |
+    sort |
+    $python_ver process_reduce.py > output.txt
+
+    read -r line < output.txt # reads first line of output.txt
+    first_nine=${line:0:9}    # FinalRank is 9 characters
+
+    if [ "$first_nine" = "FinalRank" ] # Converged
+    then
+        echo "Converged!"
+        break
+    else
+        echo "Try again"
+        input=output.txt  # Pipe output to pagerank_map
+    fi
+
+    ((i++))
 done
