0c405b059e50be8e4e08e008eda5f5f326550ea1
Jessica Choi <choi.jess816@gmail.com>
2018-02-04 22:19:55 -0800

    Implement function to parse the input

--- data/pagerank_map.py
+++ data/pagerank_map.py
@@ -6,6 +6,17 @@ import sys
 # This program simply represents the identity function.
 #
 
-for line in sys.stdin:
-    sys.stdout.write(line)
+# we want to parse the data into usable components in this file
 
+def parseData():
+    for line in sys.stdin:
+        splitLine = line.split("\t")
+        nodeId = splitLine[0].split(":")[1]
+        curr = splitLine[1].split(",")[0]
+        prev = splitLine[1].split(",")[1]
+        outlinks = splitLine[1].split(",")[2:]
+    
+        outlinksString = ",".join(outlinks)
+        sys.stdout.write("nodeId:" + nodeId + " curr:" + curr + " prev:" + prev + " outlinks: " + outlinksString + "\n")
+    
+parseData()
