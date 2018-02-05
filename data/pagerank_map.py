2d81836554433dfcf0235b5211f551d67ed263cc
Jessica Choi <choi.jess816@gmail.com>
2018-02-05 00:19:26 -0800

    Implementation of first reduce step
    Using the key-sorted outputs from the map step, we collect
    the values into lists and then output the sums

--- data/pagerank_map.py
+++ data/pagerank_map.py
@@ -6,17 +6,31 @@ import sys
 # This program simply represents the identity function.
 #
 
-# we want to parse the data into usable components in this file
-
+# we want to parse the data into usable components
+    
 def parseData():
     for line in sys.stdin:
         splitLine = line.split("\t")
         nodeId = splitLine[0].split(":")[1]
-        curr = splitLine[1].split(",")[0]
-        prev = splitLine[1].split(",")[1]
-        outlinks = splitLine[1].split(",")[2:]
-    
+        data = splitLine[1].strip().split(",")
+        # TODO: figure out how to not hardcode this
+        if data[0] == "1.0":
+            iteration = 0
+            curr = float(data[0])
+            prev = float(data[1])
+            outlinks = data[2:]
+        else:
+            iteration = int(data[0])
+            curr = float(data[1])
+            prev = float(data[2])
+            outlinks = data[3:]
+            
         outlinksString = ",".join(outlinks)
-        sys.stdout.write("nodeId:" + nodeId + " curr:" + curr + " prev:" + prev + " outlinks: " + outlinksString + "\n")
-    
+        if len(outlinks) == 0:
+            sys.stdout.write("%s, %f \n" % (nodeId, curr))
+        else:
+            for neighbor in outlinks:
+                sys.stdout.write("%s, %f \n" % (neighbor, curr/len(outlinks)))
+        # sys.stdout.write("nodeId: %s, iteration: %i, curr: %f, prev: %f, outlinks: %s \n" % (nodeId, iteration, curr, prev, outlinksString))
+        
 parseData()
--- data/pagerank_reduce.py
+++ data/pagerank_reduce.py
@@ -1,11 +1,47 @@
 #!/usr/bin/env python
 
 import sys
+import numpy as np
 
 #
 # This program simply represents the identity function.
 #
 
-for line in sys.stdin:
-    sys.stdout.write(line)
+# we want to calculate the new ranks
 
+alpha = 0.85
+
+def parseData():
+    prevId = None
+    values = []
+    for line in sys.stdin:
+        splitLine = line.split(",")
+        nodeId = splitLine[0]
+        value = float(splitLine[1])
+        
+        if prevId == None:
+            prevId = nodeId
+            values.append(value)
+        elif nodeId == prevId:
+            values.append(value)
+        else:
+            calculateSum(prevId, values)
+            prevId = nodeId
+            values = [value]
+                                
+def calculateSum(nodeId, values):
+    values_sum = np.sum(values)
+    curr = alpha * values_sum + (1-alpha)
+    sys.stdout.write("%s, %f \n" % (nodeId, curr))
+
+                  
+parseData()
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
