95b15e2c020372225b1613c4ef7aa42de4439a2a
Jessica Choi <choi.jess816@gmail.com>
2018-02-06 14:13:03 -0800

    Works on Amazon EMR cluster now
    Cast nodeId in process_reduce to int
    Set isConverged to be None initially, and changed to False
    or True after iterating through priority queue, so that if the
    queue is empty/null because of nonsense/blank input lines,
    we do not write any lines

---
+++ results/pagerank_map.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import sys
+
+#
+# This program simply represents the identity function.
+#
+
+# we want to parse the data into usable components
+
+# def parseData():
+# First line input: (nodeId \t current, previous, neighbors)
+# Every other line: (nodeId \t iteration, current, previous, neighbors)
+for line in sys.stdin:
+    splitLine = line.split("\t")
+    nodeId = splitLine[0].split(":")[1]
+    assert(nodeId is not None)
+    data = splitLine[1].strip().split(",")
+    # If this is a float
+    if '.' in data[0]:
+        iteration = -1
+        curr = float(data[0])
+        outlinks = data[2:]
+    # If this is a non-float iteration value
+    else:
+        iteration = int(data[0])
+        curr = float(data[1])
+        outlinks = data[2:]
+        
+    outlinksString = ",".join(outlinks)
+
+
+    # Other output is simply (node, amountOfRankToAddToNode)
+
+    lengthOutlinks = len(outlinks)
+    # If there are no outlinks 
+    if lengthOutlinks == 0:
+        sys.stdout.write("%s\t%f\n" % (nodeId, curr))
+        sys.stdout.write("NodeId:%s\t%i,%f\n" % (nodeId, iteration, curr))
+    else:
+        for neighbor in outlinks:
+            # For each line, we need to pass on the information of previous, the current iteration,
+            # and neighbors
+            sys.stdout.write("%s\t%f\n" % (neighbor, curr/lengthOutlinks))
+        sys.stdout.write("NodeID:%s\t%i,%f,%s\n" % (nodeId, iteration, curr, outlinksString))
+    
+# parseData()
---
+++ results/pagerank_process.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+i=0
+n_iterations=50
+input=input.txt
+python_ver=python # Change this to however you invoke python
+
+while [ $i -lt $n_iterations ]
+do
+    $python_ver pagerank_map.py < $input |
+    sort |
+    $python_ver pagerank_reduce.py |
+    $python_ver process_map.py |
+    sort |
+    $python_ver process_reduce.py > output.txt
+
+    read -r line < output.txt # reads first line of output.txt
+    first_nine=${line:0:9}    # FinalRank is 9 characters
+    echo "$i"        # Prints first_nine
+
+    if [ "$first_nine" = "FinalRank" ] # Converged
+    then
+        echo "Converged!"
+        break
+    else
+        echo "Try again"
+        input=output.txt  # Pipe output to pagerank_map
+    fi
+
+    ((i++))
+done
