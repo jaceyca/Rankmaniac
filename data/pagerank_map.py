c059ba851b43dedd0ee605da00c628b8b4b24bcf
Jessica Choi <choi.jess816@gmail.com>
2018-02-06 15:11:13 -0800

    Merged relevant files

--- data/pagerank_map.py
+++ data/pagerank_map.py
@@ -7,42 +7,41 @@ import sys
 #
 
 # we want to parse the data into usable components
-    
-def parseData():
-    # First line input: (id \t current, previous, neighbors)
-    # Every other line: (id \t iteration, current, previous, neighbors)
-    for line in sys.stdin:
-        splitLine = line.split("\t")
-        nodeId = splitLine[0].split(":")[1]
-        data = splitLine[1].strip().split(",")
-        # If this is a float
-        if '.' in data[0]:
-            iteration = -1
-            curr = float(data[0])
-            outlinks = data[2:]
-        # If this is a non-float iteration value
-        else:
-            iteration = int(data[0])
-            curr = float(data[1])
-            outlinks = data[2:]
-            
-        outlinksString = ",".join(outlinks)
 
-
-        # Other output is simply (node, amountOfRankToAddToNode)
-
-        lengthOutlinks = len(outlinks)
-        # If there are no outlinks 
-        if lengthOutlinks == 0:
-            sys.stdout.write("NodeId:%s\t%i,%f\n" % (nodeId, iteration, curr))
-            sys.stdout.write("%s\t%f\n" % (nodeId, curr))
-        else:
-            for neighbor in outlinks:
-                # For each line, we need to pass on the information of previous, the current iteration,
-                # and neighbors
-                sys.stdout.write("NodeID:%s\t%i,%f,%s\n" % (nodeId, iteration, curr, outlinksString))
-
-                sys.stdout.write("%s\t%f\n" % (neighbor, curr/lengthOutlinks))
-        # sys.stdout.write("nodeId: %s, iteration: %i, curr: %f, prev: %f, outlinks: %s \n" % (nodeId, iteration, curr, prev, outlinksString))
+# def parseData():
+# First line input: (nodeId \t current, previous, neighbors)
+# Every other line: (nodeId \t iteration, current, previous, neighbors)
+for line in sys.stdin:
+    splitLine = line.split("\t")
+    nodeId = splitLine[0].split(":")[1]
+    assert(nodeId is not None)
+    data = splitLine[1].strip().split(",")
+    # If this is a float
+    if '.' in data[0]:
+        iteration = -1
+        curr = float(data[0])
+        outlinks = data[2:]
+    # If this is a non-float iteration value
+    else:
+        iteration = int(data[0])
+        curr = float(data[1])
+        outlinks = data[2:]
         
-parseData()
+    outlinksString = ",".join(outlinks)
+
+
+    # Other output is simply (node, amountOfRankToAddToNode)
+
+    lengthOutlinks = len(outlinks)
+    # If there are no outlinks 
+    if lengthOutlinks == 0:
+        sys.stdout.write("%s\t%f\n" % (nodeId, curr))
+        sys.stdout.write("NodeId:%s\t%i,%f\n" % (nodeId, iteration, curr))
+    else:
+        for neighbor in outlinks:
+            # For each line, we need to pass on the information of previous, the current iteration,
+            # and neighbors
+            sys.stdout.write("%s\t%f\n" % (neighbor, curr/lengthOutlinks))
+        sys.stdout.write("NodeID:%s\t%i,%f,%s\n" % (nodeId, iteration, curr, outlinksString))
+    
+# parseData()
--- data/pagerank_reduce.py
+++ data/pagerank_reduce.py
@@ -11,34 +11,35 @@ import sys
 
 alpha = 0.85
 
-def parseData():
-    prevId = None
-    totalDegreeChange = 0
-    for line in sys.stdin:
-        # If the line starts with N as in NodeID:..., then we know it's passthrough information
-        # and therefore just pass it on
-        if line[0] == 'N':
-            line = line[7:]
-            sys.stdout.write(line)
-            continue
-
+# def parseData():
+prevId = -2
+totalRankChange = 0.0
+newRank = 0.0
+for line in sys.stdin:
+    # If the line starts with N as in NodeID:..., then we know it's passthrough information
+    # and therefore just pass it on
+    if line[0] == 'N':
+        line = line[7:]
+        sys.stdout.write(line)
+    else:
         # Now, if it's data of the form (node, amountOfRankToAddToNode)
         splitLine = line.split("\t")
         assert(len(splitLine) == 2)
         nodeId = splitLine[0]
-        degreeChange = float(splitLine[1])
+        rankChange = float(splitLine[1])
         # If this is the first line
-        if prevId is None:
+        if prevId == -2:
             prevId = nodeId
-            totalDegreeChange += degreeChange
-        elif prevId == nodeId:
-            totalDegreeChange += degreeChange
-        else: # we have a new ID
-            newDegree = alpha * totalDegreeChange + (1-alpha)   
-            sys.stdout.write("%s\t%f\n" % (prevId, newDegree))
-            totalDegreeChange = degreeChange
+        elif prevId != nodeId:
+            # we have a new ID
+            newRank = alpha * totalRankChange + (1-alpha)   
+            sys.stdout.write("%s\t%f\n" % (prevId, newRank))
+            totalRankChange = 0.0
             prevId = nodeId
-    newDegree = alpha * totalDegreeChange + (1-alpha)   
-    sys.stdout.write("%s\t%f\n" % (prevId, newDegree))
-                                       
-parseData()
+        totalRankChange += rankChange
+if prevId != -2:
+    assert (prevId is not None)
+    newRank = alpha * totalRankChange + (1-alpha)   
+    sys.stdout.write("%s\t%f\n" % (prevId, newRank))
+                                   
+# parseData()
